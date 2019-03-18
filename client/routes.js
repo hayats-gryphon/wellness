760e9e38961b449ebd244adbf8e7216ada24b36a
freetimealice <39662768+freetimealice@users.noreply.github.com>
2019-03-18 17:48:44 -0400

    Sound (#26)
    * feat(visuals): added end game page and bugs to swat
    
    * feat(sound): added swat sound
    
    * feat(sound): plays bg music
    
    * feat(sound): plays bg music

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-
 import {Login, Signup, UserHome, PunchABug, EndOfGame, EntrancePage} from './components'
 
 import {me} from './store'
--- client/store/board.js
+++ client/store/board.js
@@ -2,18 +2,21 @@
  * ACTION TYPES
  */
 const GOT_MOLES = 'GOT_MOLES'
+const VIDEO_LOADED = 'VIDEO_LOADED'
 
 /**
  * INITIAL STATE
  */
 const initialState = {
-  moles: []
+  moles: [],
+  videoLoaded: false
 }
 
 /**
  * ACTION CREATORS
  */
 export const gotMoles = moles => ({type: GOT_MOLES, moles})
+export const videoLoaded = () => ({type: VIDEO_LOADED})
 
 /**
  * REDUCER
@@ -22,6 +25,8 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GOT_MOLES:
       return {...state, moles: action.moles}
+    case VIDEO_LOADED:
+      return {...state, videoLoaded: true}
     default:
       return state
   }
