f69609972ef6ad22cd14c02e58206142f530cdc7
Alexandra Beautyman <alexandra.beautyman@gmail.com>
2019-03-20 11:54:44 -0400

    feat(persistence): added highscore to user table

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -15,6 +15,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           <a href="#" onClick={handleClick}>
             Logout
           </a>
+          <Link to="/punchabug">Puncha Bug</Link>
         </div>
       ) : (
         <div>
