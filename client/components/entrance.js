247feb6e94db9bf25afa6ef63dedc434ab5cb6b3
Christine Kim <chrismekim@gmail.com>
2019-03-20 11:31:09 -0400

    feat(entrance,navbar): added entrance background image

--- client/components/entrance.js
+++ client/components/entrance.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+// import startScreen from  '../../public/images/start-screen.png'
+// import logo from '../../public/images/logo.png'
+// import playButton from '../../public/images/play-button.png'
+
+//
+
 /**
  * COMPONENT
  */
+
+// const styles = {
+//   entrancePageContainer: {
+//     width: '88%',
+//     height: '800px',
+//     background: "images/start-screen.png",
+//     backgroundSize: 'cover'
+//   }
+// };
+
 export default class EntracePage extends React.Component {
   render() {
     return (
-      <div>
-        <h3>Welcome</h3>
-        <Link to="/punchabug">
-          <button>Let Us Play</button>
-        </Link>
+      <div className="entrance-container">
+        <div className="play-button">
+          <Link to="/punchabug">
+            <img src="images/play-button.png" />
+          </Link>
+        </div>
       </div>
     )
   }
