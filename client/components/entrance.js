d0fe40d5fe259e771294ec895e31fb45beb9d623
Christine Kim <chrismekim@gmail.com>
2019-03-20 11:40:05 -0400

    fix(entrance): removed commented out code

--- client/components/entrance.js
+++ client/components/entrance.js
@@ -1,23 +1,9 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-// import startScreen from  '../../public/images/start-screen.png'
-// import logo from '../../public/images/logo.png'
-// import playButton from '../../public/images/play-button.png'
-
-//
 
 /**
  * COMPONENT
  */
-
-// const styles = {
-//   entrancePageContainer: {
-//     width: '88%',
-//     height: '800px',
-//     background: "images/start-screen.png",
-//     backgroundSize: 'cover'
-//   }
-// };
 
 export default class EntracePage extends React.Component {
   render() {
