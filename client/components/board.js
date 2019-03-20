6e6040be2e37d1c39ad1eefdfa24cdbe9d6a269e
AlexandraBeautyman <45341190+AlexandraBeautyman@users.noreply.github.com>
2019-03-20 15:16:47 -0400

    Update board.js

--- client/components/board.js
+++ client/components/board.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {gotHoles} from '../store/board'
 import Hole from './hole'
-import {Route, Redirect, withRouter} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 
 class Board extends React.Component {
   constructor(props) {
