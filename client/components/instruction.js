d8e0bcb75ca4c970c3aec2325795638539e6b34d
June Hosoya <junehosoya@gmail.com>
2019-03-23 15:04:28 -0400

    feat:(instruction/css): finished with instruction component and css

--- client/components/instruction.js
+++ client/components/instruction.js
@@ -9,19 +9,21 @@ export default class Instruction extends React.Component {
       <div className="outer-div">
         <div className="entrance-container">
           <h1 className="how-to">HOW TO PLAY</h1>
-          <div className="instruction">
-            <h2>Beginner</h2>
-            <p>Hit all the bugs with your nose!</p>
-            <h2>Medium</h2>
-            <p>
-              Hit all the bugs but be careful not to hit the Lady flower! Your
-              points will be deducted every time you hit the Lady flower.
-            </p>
-            <h2>Hard</h2>
-            <p>
-              Hit all the bugs and watch out for Golden Bug! You can earn extra
-              10 points hitting the Golden Bug.
-            </p>
+          <div className="instruction-container">
+            <div className="instruction">
+              <h2>BEGINNER</h2>
+              <p>Hit all the bugs with your nose!</p>
+              <h2>MEDIUM</h2>
+              <p>
+                Hit all the bugs but be careful not to hit the Lady flower! Your
+                points will be deducted every time you hit the Lady flower.
+              </p>
+              <h2>HARD</h2>
+              <p>
+                Hit all the bugs and watch out for Golden Bug! You can earn
+                extra 10 points hitting the Golden Bug.
+              </p>
+            </div>
           </div>
         </div>
         <button type="button" name="back-button">
