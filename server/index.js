72f86965948e97cb66b83f737af0d419c412513a
Alexandra Beautyman <alexandra.beautyman@gmail.com>
2019-03-25 17:14:04 -0400

    chore(deployed database): temporarily forcing true in order to get deployed database up-to-date, closes #129

--- server/index.js
+++ server/index.js
@@ -109,7 +109,7 @@ const startListening = () => {
   require('./socket')(io)
 }
 
-const syncDb = () => db.sync()
+const syncDb = () => db.sync({force: true})
 
 async function bootApp() {
   await sessionStore.sync()
