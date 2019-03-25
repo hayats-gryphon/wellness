e9086d73eb959b045156e72a4c494db592f9d3e9
freetimealice <39662768+freetimealice@users.noreply.github.com>
2019-03-25 16:38:21 -0400

    Footer placement (#114)
    * feat(footer-nav-placement) positions menu buttons
    
    * feat(footer-nav-placement) positions menu buttons
    
    * feat(footer-nav-placement) positions menu buttons
    
    * feat(footer-nav-placement) positions menu buttons
    
    * update logout spec

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -34,7 +34,6 @@ const AuthForm = props => {
           </div>
           {error && error.response && <div> {error.response.data} </div>}
         </form>
-        <a href="/auth/google">{displayName} with Google</a>
       </div>
     </div>
   )
