042ab970be6a356087e999442c95f9888c787512
Alexandra Beautyman <alexandra.beautyman@gmail.com>
2019-03-26 17:24:28 -0400

    feat(about page): created a basic about page; wired it up through the footer

---
+++ client/components/about.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Punchabug was built by Christine Kim, June Hosoya, Alice He, and
+        Alexandra Beautyman as a Capstone project for the Grace Hopper Program
+        at Fullstack Academy of Code.
+      </p>
+      <p>
+        We used PostgresSql, Sequelize.js, Node.js, Express.js, React.js,
+        Redux.js, HTML, and CSS, as well as TensorFlow's PoseNet, a pre-trained
+        machine learning model for realtime human pose estimation in the
+        browser.
+      </p>
+      <p>Special thanks are owed to the creators of our images and sounds:</p>
+      <ul>
+        <li>
+          Leaf icon made by{' '}
+          <a
+            href="http://www.freepik.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Freepik
+          </a>{' '}
+          from{' '}
+          <a
+            href="http://www.flaticon.com"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            www.flaticon.com
+          </a>
+        </li>
+        <li>
+          Flower icon made by{' '}
+          <a
+            href="http://www.freepik.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Freepik
+          </a>{' '}
+          from{' '}
+          <a
+            href="http://www.flaticon.com"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            www.flaticon.com
+          </a>
+        </li>
+        <li>
+          Ladybug icon made by Rwida A. Kamel from{' '}
+          <a
+            href="http://www.pngtree.com"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            www.pngtree.com
+          </a>
+        </li>
+        <li>
+          Lightning bug icon from the public domain, sourced from by{' '}
+          <a
+            href="http://www.clker.com"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            www.clker.com
+          </a>
+        </li>
+        <li>
+          Music and sounds by{' '}
+          <a
+            href="https://soundcloud.com/map_composer/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Pierre Malleret
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default About
--- client/routes.js
+++ client/routes.js
@@ -10,6 +10,7 @@ import {
   EntrancePage,
   Menu,
   Instruction,
+  About,
   Loading
 } from './components'
 
@@ -44,6 +45,7 @@ class Routes extends Component {
         <Route path="/end-of-game" component={EndOfGame} />
         <Route path="/instruction" component={Instruction} />
         <Route path="/menu" component={Menu} />
+        <Route path="/about" component={About} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
